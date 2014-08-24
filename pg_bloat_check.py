c8c7702511db498ece7ea2ebd9faa4c7703ac120
Keith Fiske <keith@keithf4.com>
2014-08-24 10:00:25 -0400

    Add filter for minimum wasted size. Add dictionary output format.

--- pg_bloat_check.py
+++ pg_bloat_check.py
@@ -5,20 +5,21 @@ from psycopg2 import extras
 
 # Bloat queries are adapted from the check_bloat query found in bucardo's check_postgres tool http://bucardo.org/wiki/Check_postgres
 
-parser = argparse.ArgumentParser(description="Provide a bloat report for PostgreSQL tables and/or indexes.")
+parser = argparse.ArgumentParser(description="Provide a bloat report for PostgreSQL tables and/or indexes. Note that the query to check for bloat can be extremely expensive on very large databases or those with very large tables. The materialized view option in 9.3+ can help to mitigate getting bloat data back by storing it persistently instead of running a query every time the script is run (see --create_mat_view).")
 args_general = parser.add_argument_group(title="General options")
-args_general.add_argument('--mode', '-m', choices=["tables", "indexes"], default="tables", help="""Provide bloat report for the following objects: tables, indexes. Note that the "tables" mode does not include any index bloat that may also exist in the table. Default is "tables".""")
+args_general.add_argument('-m', '--mode', choices=["tables", "indexes"], default="tables", help="""Provide bloat report for the following objects: tables, indexes. Note that the "tables" mode does not include any index bloat that may also exist in the table. Default is "tables".""")
 args_general.add_argument('-c','--connection', default="host=", help="""Connection string for use by psycopg. Defaults to "host=" (local socket).""")
-#args_general.add_argument('-f', '--format', default="table", choices=["table", "simple", "dict"], help="Output formats. Table outputs a table format sutable for a console. Simple is a plaintext version suitable for any output (preferred setting for email). Dict is a python dictionary object, which may be useful if taking input into another python script or something that needs a more structured format.")
+# TODO see about a possible table format
+args_general.add_argument('-f', '--format', default="simple", choices=["simple", "dict"], help="Output formats. Simple is a plaintext version suitable for any output (ex: console, pipe to email). Dict is a python dictionary object, which may be useful if taking input into another python script or something that needs a more structured format. Dict also provides more details about object pages. Default is simple.")
 args_general.add_argument('-a', '--min_pages', type=int, default=1, help="Minimum number of pages an object must have to be included in the report. Default and minimum value is 1.")
 args_general.add_argument('-A', '--min_wasted_pages', type=int, default=1, help="Minimum number of wasted pages an object must have to be included in the report. Default and minimum value is 1.")
-args_general.add_argument('-p', '--min_wasted_percentage', type=float, default=0.1, help="Minimum percentage of wasted space an object must have to be included in the report. Default value is 0.1%%")
+args_general.add_argument('-p', '--min_wasted_percentage', type=float, default=0.1, help="Minimum percentage of wasted space an object must have to be included in the report. Default value is 0.1%% and given values cannot be any less than this.")
+args_general.add_argument('-z', '--min_wasted_size', type=long, default=0, help="Minimum size of wasted space in bytes. Default is zero.")
 args_general.add_argument('-n', '--schema', help="Comma separated list of schema to include in report. All other schemas will be ignored.")
-args_general.add_argument('-N', '--exclude_schema', help="Comma separated list of schemas to exclude. If set with -n, schemas will be excluded then included.")
-args_general.add_argument('--view_schema', help="Set the schema that the bloat report view is in if it's not in the default search_path. Note this option can also be set when running --create_view to set in which schema you want the view created.")
-args_general.add_argument('-q', '--quiet', action="store_true", help="Stop all output to the console.")
+args_general.add_argument('-N', '--exclude_schema', help="Comma separated list of schemas to exclude. If set along with -n, schemas will be excluded then included.")
 
 args_setup = parser.add_argument_group(title="Setup")
+args_general.add_argument('--view_schema', help="Set the schema that the bloat report view is in if it's not in the default search_path. Note this option can also be set when running --create_view to set in which schema you want the view created.")
 args_setup.add_argument('--create_view', action="store_true", help="Create the required view that the bloat report uses. Places view in default search_path schema unless --view_schema is set.")
 args_setup.add_argument('--create_mat_view', action="store_true", help="Same as --create_view, but creates it as materialized view if your version of PostgreSQL supports it (9.3+). Be aware that this script does not refresh the materialized view automatically.")
 args = parser.parse_args()
@@ -114,6 +115,45 @@ def create_view(conn):
     cur.close()
 
 
+def get_index_bloat(conn):
+    sql = """
+    SELECT
+        current_database() AS db
+        , schemaname
+        , iname as objectname
+        , ipages::bigint AS pages
+        , ROUND(CASE 
+                    WHEN iotta=0 AND ipages>0 THEN 100.0
+                    WHEN iotta=0 OR ipages=0 OR ipages=iotta THEN 0.0 
+                    ELSE ((ipages-iotta)::numeric/ipages)*100
+                END
+            , 1) AS bloat_percent
+        , CASE 
+            WHEN ipages < iotta THEN 0 
+            ELSE ipages::bigint - iotta 
+          END AS wastedpages::bigint
+        , CASE 
+            WHEN ipages < iotta THEN 0 
+            ELSE bs*(ipages-iotta) 
+          END AS wastedbytes::bigint
+        , CASE 
+            WHEN ipages < iotta THEN '0 bytes' 
+            ELSE pg_size_pretty((bs*(ipages-iotta))::bigint)
+          END AS wastedsize
+    FROM """
+    if args.view_schema != None:
+        sql += args.view_schema + "."
+    sql += """bloat_view
+    WHERE iname <> '?' 
+    AND ipages > 1
+    AND (ipages-iotta) > 1
+    ORDER BY wastedbytes DESC, schemaname ASC, tablename ASC"""
+    cur = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
+    cur.execute(sql)
+    result = cur.fetchall()
+    return result
+
+
 def get_table_bloat(conn):
     sql = """
     SELECT
@@ -146,45 +186,6 @@ def get_table_bloat(conn):
     AND relpages > 1
     AND (relpages-otta) > 0
     ORDER BY wastedbytes DESC, schemaname ASC, tablename ASC"""
-    cur = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
-    cur.execute(sql)
-    result = cur.fetchall()
-    return result
-
-
-def get_index_bloat(conn):
-    sql = """
-    SELECT
-        current_database() AS db
-        , schemaname
-        , iname as objectname
-        , ipages::bigint AS pages
-        , ROUND(CASE 
-                    WHEN iotta=0 AND ipages>0 THEN 100.0
-                    WHEN iotta=0 OR ipages=0 OR ipages=iotta THEN 0.0 
-                    ELSE ((ipages-iotta)::numeric/ipages)*100
-                END
-            , 1) AS bloat_percent
-        , CASE 
-            WHEN ipages < iotta THEN 0 
-            ELSE ipages::bigint - iotta 
-          END AS wastedpages
-        , CASE 
-            WHEN ipages < iotta THEN 0 
-            ELSE bs*(ipages-iotta) 
-          END AS wastedbytes
-        , CASE 
-            WHEN ipages < iotta THEN '0 bytes' 
-            ELSE pg_size_pretty((bs*(ipages-iotta))::bigint)
-          END AS wastedsize
-    FROM """
-    if args.view_schema != None:
-        sql += args.view_schema + "."
-    sql += """bloat_view
-    WHERE iname <> '?' 
-    AND ipages > 1
-    AND (ipages-iotta) > 1
-    ORDER BY wastedbytes DESC, schemaname ASC, iname ASC"""
     cur = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
     cur.execute(sql)
     result = cur.fetchall()
@@ -227,14 +228,32 @@ if __name__ == "__main__":
     for r in result:
         # Min check goes in order page, wasted_page, wasted_percentage to exclude things properly when options are combined
         if args.min_pages > 1:
-            if r['pages'] <= args.min_pages:
+            if r['pages'] < args.min_pages:
                 continue
+        elif args.min_pages < 1:
+            print("--min_pages (-a) must be >= 1")
+            sys.exit(2)
+
         if args.min_wasted_pages > 1:
-            if r['wastedpages'] <= args.min_wasted_pages:
+            if r['wastedpages'] < args.min_wasted_pages:
                 continue
+        elif args.min_wasted_pages < 1:
+            print("--min_wasted_pages (-A) must be >= 1")
+            sys.exit(2)
+
         if args.min_wasted_percentage > 0.1:
-            if r['bloat_percent'] <= args.min_wasted_percentage:
+            if r['bloat_percent'] < args.min_wasted_percentage:
                 continue
+        elif args.min_wasted_percentage < 0.1:
+            print("--min_wasted_percentage (-p) must be >= 0.1%%")
+            sys.exit(2)
+
+        if args.min_wasted_size > 1:
+            if r['wastedbytes'] < args.min_wasted_size:
+                continue
+        elif args.min_wasted_size < 1:
+            print("--min_wasted_size (-z) must be >= 1")
+            sys.exit(2)
 
         if r['schemaname'] in exclude_schema_list:
             continue
@@ -243,11 +262,19 @@ if __name__ == "__main__":
             if r['schemaname'] not in include_schema_list:
                 continue
 
-#        if args.format == "simple":
-        justify_space = 100 - len(str(counter)+". "+r['schemaname']+"."+r['objectname']+"(%)"+str(r['bloat_percent'])+r['wastedsize']+" wasted")
-        result_list.append(str(counter) + ". " + r['schemaname'] + "." + r['objectname'] + "."*justify_space + "(" + str(r['bloat_percent']) + "%) " + r['wastedsize'] + " wasted")
-        counter += 1
+        if args.format == "simple":
+            justify_space = 100 - len(str(counter)+". "+r['schemaname']+"."+r['objectname']+"(%)"+str(r['bloat_percent'])+r['wastedsize']+" wasted")
+            result_list.append(str(counter) + ". " + r['schemaname'] + "." + r['objectname'] + "."*justify_space + "(" + str(r['bloat_percent']) + "%) " + r['wastedsize'] + " wasted")
+            counter += 1
+        elif args.format == "dict":
+            result_dict = dict([('schemaname', r['schemaname'])
+                                , ('objectname', r['objectname'])
+                                , ('total_pages', int(r['pages']) )
+                                , ('bloat_percent', str(r['bloat_percent'])+"%" )
+                                , ('wasted_size', r['wastedsize'])
+                                , ('wasted_pages', r['wastedpages'])
+                                ])
+            result_list.append(result_dict)
 
-    if not args.quiet:
-        print_report(result_list)
+    print_report(result_list)
 
