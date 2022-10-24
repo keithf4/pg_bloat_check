058e912924a837987e5a68cd6193e6ede30871e2
Keith Fiske <keith@keithf4.com>
2022-10-24 17:16:15 -0400

    v2.7.1 Update version number

--- pg_bloat_check.py
+++ pg_bloat_check.py
@@ -6,7 +6,7 @@ import argparse, csv, json, psycopg2, re, sys
 from psycopg2 import extras
 from random import randint
 
-version = "2.7.0"
+version = "2.7.1"
 
 parser = argparse.ArgumentParser(description="Provide a bloat report for PostgreSQL tables and/or indexes. This script uses the pgstattuple contrib module which must be installed first. Note that the query to check for bloat can be extremely expensive on very large databases or those with many tables. The script stores the bloat stats in a table so they can be queried again as needed without having to re-run the entire scan. The table contains a timestamp columns to show when it was obtained.")
 args_general = parser.add_argument_group(title="General options")
